a0810152de94daa2bc63c28117fbd1f0f38499ee
Zach Quinn <zacharytq@gmail.com>
2021-10-16 14:52:09 -0400

    testing out mutability

---
+++ variables/target/debug/deps/variables-287726166acab161.d
@@ -0,0 +1,5 @@
+/home/zach/Desktop/rust_book/variables/target/debug/deps/variables-287726166acab161: src/main.rs
+
+/home/zach/Desktop/rust_book/variables/target/debug/deps/variables-287726166acab161.d: src/main.rs
+
+src/main.rs:
